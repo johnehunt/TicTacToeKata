ec13f1a7d0ac2a1702e5738d4c1ef8961784f953
John Hunt <john.hunt10@gmail.com>
2022-02-02 17:05:04 +0000

    Step 1

---
+++ src/main/java/com/jjh/tictactoe/Board.java
@@ -0,0 +1,14 @@
+package com.jjh.tictactoe;
+
+public class Board {
+
+  public static final String X = "X";
+  public static final String Y = "O";
+
+  private final String[][] cells = new String[3][3];
+
+  public boolean isEmpty() {
+    return true;
+  }
+
+}
---
+++ src/test/java/com/jjh/tictactoe/BoardTest.java
@@ -0,0 +1,29 @@
+package com.jjh.tictactoe;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class BoardTest {
+
+  private Board board;
+
+  @BeforeEach
+  void setUp() {
+    board = new Board();
+  }
+
+  @AfterEach
+  void tearDown() {
+    board = null;
+  }
+
+  @Test
+  void isBoardEmpty() {
+    boolean result = board.isEmpty();;
+    assertThat("the board should be empty", result, equalTo(true));
+  }
+}
